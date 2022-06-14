8bd5f39fae149340e0045f7604f0ddb1c3e18f73
Hardik Agarwal <hardikagarwal1703@gmail.com>
2022-06-15 00:16:10 +0530

    Q2 Validation Left
    Validation left

---
+++ day10/Q2 & Q4/js/q4.js
@@ -0,0 +1,22 @@
+function checkForEmpty(){
+    if(document.getElementById("mId").value == null || document.getElementById("mId").value == "" ){
+        document.getElementById("idI").innerText = "required *"
+        return;
+    }
+
+    if(document.getElementById("mName").value == null || document.getElementById("mName").value == "" ){
+        document.getElementById("mNameI").innerText = "required *"
+        return;
+    }
+
+    if(document.getElementById("mId").value == null || document.getElementById("mId").value == "" ){
+        document.getElementById("idI").innerText = "required *"
+        return;
+    }
+
+    if(document.getElementById("mId").value == null || document.getElementById("mId").value == "" ){
+        document.getElementById("idI").innerText = "required *"
+        return;
+    }
+
+}
