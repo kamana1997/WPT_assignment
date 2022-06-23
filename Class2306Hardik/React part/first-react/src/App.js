3010baa948187b31f1a34b086315cf9e778f19d2
Hardik Agarwal <hardikagarwal1703@gmail.com>
2022-06-23 10:45:22 +0530

    ok

--- Class2306Hardik/React part/first-react/src/App.js
+++ Class2306Hardik/React part/first-react/src/App.js
@@ -1,11 +1,12 @@
 //dont change name App keep it default (recommendation)
 function App(){
+  var name = "Hardik Agarwal";
   return(
     //here we will write our html code with one tag
     <div>
-      <h1>Good Morning</h1>
+      <h1>Good Morning {name} </h1>
     </div>
-  )
+  );
 }
 
 export default App;
