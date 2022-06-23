e632ab71d7d9ec6bd1fb93767f92fb432a023f08
Hardik Agarwal <hardikagarwal1703@gmail.com>
2022-06-23 11:42:06 +0530

    added state

--- Class2306Hardik/React part/first-react/src/CalcComponents/Calc.js
+++ Class2306Hardik/React part/first-react/src/CalcComponents/Calc.js
@@ -1,11 +1,17 @@
+import {useState} from "react"
 function Calc(){
+    //var result = 0;
+    var [result,setResult] = useState(0);
     var calculate = function (event){
         event.preventDefault(); //stops form default action
         var num1 = event.target.num1.value;
         var num2 = event.target.num2.value;
 
-        var result = parseInt(num1) + parseInt(num2);
-        console.log(result);
+        var x = parseInt(num1) + parseInt(num2);
+        console.log(x);
+
+        //Setting the value to result state variable
+        setResult(x);
     }
     return(
         <div>
@@ -14,6 +20,8 @@ function Calc(){
             <input type = "text" name = "num2" placeholder="Enter Number"/>
             <button>Calculate</button>
             </form>
+            {/*Here we are fetching the value of result state variable*/}
+            <h3>Result is {result}</h3>
         </div>
         
     );
