7fcd47ab0411cac6602d5fd26820582f8d59a72f
Hardik Agarwal <hardikagarwal1703@gmail.com>
2022-06-26 09:54:21 +0530

    .

--- Class2606Hardik/class2606/src/Redux_Demo/Third.js
+++ Class2606Hardik/class2606/src/Redux_Demo/Third.js
@@ -1,7 +1,12 @@
+import { useSelector } from "react-redux";
+
 export default function Third(){
+    var fruit_name = useSelector((state) => state.fruitName.fruit_name);
+    
     return(
       <div>
-        Good Morning  !
+        <br />
+        <h1>Good Morning {fruit_name}</h1>
       </div>
     );
   }
